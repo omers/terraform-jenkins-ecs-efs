8ce0e28524ff557fa880cc5910b2c940810b3766
Omer Segev <omersegev@gmail.com>
2022-11-16 10:06:59 +0200

    Changed ALB health check interval

--- alb.tf
+++ alb.tf
@@ -51,7 +51,7 @@ resource "aws_lb_target_group" "target_group" {
 
   health_check {
     healthy_threshold   = "3"
-    interval            = "3000"
+    interval            = "30"
     protocol            = "HTTP"
     matcher             = "200"
     timeout             = "3"
--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      #version = "2.70.0"
     }
   }
 
@@ -17,6 +16,4 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
-  #access_key = var.aws_access_key
-  #secret_key = var.aws_secret_key
 }
